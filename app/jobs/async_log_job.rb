4786c92679f2b13bd91bfa53ee328d001b551762
tarakish <akr.3838soccer14@gmail.com>
2022-09-11 13:24:50 +0900

    Feat: queueの動的振り分け

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,5 +1,12 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :async_log
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    else
+      :default
+    end
+  end
 
   def perform(message: "hello")
     AsyncLog.create!(message: message)
