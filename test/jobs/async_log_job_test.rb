478872e9db6a426a9f0fc6c48deaa8285254837e
tarakish <akr.3838soccer14@gmail.com>
2022-09-11 13:40:19 +0900

    Feat: ジョブのテストを追加

--- test/jobs/async_log_job_test.rb
+++ test/jobs/async_log_job_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
-
 class AsyncLogJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Enqueue AsyncLogJob' do
+    assert_enqueued_with(job: AyncLogJob) do
+      AsyncLogJob.perform_later(message: 'from tedst')
+    end
+  end
 end
